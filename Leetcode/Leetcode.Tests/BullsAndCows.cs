4fc42980ba9c2b847830fe760ff78fd07897f8c4
Nan Zhao <nanz@microsoft.com>
2015-11-12 21:33:08 -0800

    initial check in

---
+++ Leetcode/Leetcode.Tests/BullsAndCows.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Tests
+{
+    [TestClass]
+    public class BullsAndCows
+    {
+
+        public string GetHint(string secret, string guess)
+        {
+            var A = 0;
+            var B = 0;
+            var cnt = new int[10];
+            for (var i = 0; i < secret.Length; ++i)
+            {
+                if (secret[i] == guess[i]) ++A;
+                else
+                {
+                    if (++cnt[secret[i] - '0'] <= 0) ++B;
+                    if (--cnt[guess[i] - '0'] >= 0) ++B;
+                }
+            }
+            return A + "A" + B + "B";
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GetHint("1807", "7810");
+        }
+    }
+}
---
+++ Leetcode/Leetcode.Tests/MoveZeros.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Tests
+{
+    [TestClass]
+    public class MoveZeros
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            var i = 0;
+            while (i < nums.Length && nums[i] != 0) { i++; }
+            if (i == nums.Length) return;
+            var j = i + 1;
+            while (j < nums.Length)
+            {
+                if (nums[j] != 0)
+                {
+                    Swap(nums, i, j);
+                    i++;
+                }
+                j++;
+            }
+        }
+        public void Swap(int[] nums, int i, int j)
+        {
+            var tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        
+        }
+    }
+}
