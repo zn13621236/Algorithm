ef84d5b527ab57aab28fe86f53ff055ad99ee7ab
zn13621236 <nanzhao136@gmail.com>
2015-11-19 22:15:54 -0800

    209

--- Leetcode/Leetcode.Tests/209.cs
+++ Leetcode/Leetcode.Tests/209.cs
@@ -27,15 +27,38 @@ If you have figured out the O(n) solution, try coding another solution of which 
     public class _209
     {
 
-
-
-
+        // use 2 ponters
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            int left=0, right = left, rightMark = right;
+            var sum = 0;
+            var minLen =int.MaxValue;
+            while (left < nums.Length)
+            {
+                if (right!=0&&right == rightMark)
+                {
+                    sum -= nums[left - 1];
+                }
+                while (sum<s&&right<nums.Length)
+                {
+                    sum += nums[right];
+                    right++;
+                }
+                if (sum >= s) {
+                    minLen = Math.Min(right - left, minLen);
+                }
+                rightMark = right;
+                left++;
+            }
+            return minLen==int.MaxValue?0:minLen;
+        }
 
         [TestMethod]
-        public void testMethod()
+        public void TestMethod()
         {
 
-
+         int result=   MinSubArrayLen(3, new int[] { 1,1});
 
 
         }
---
+++ Leetcode/Leetcode.Tests/229.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Tests
+{
+
+    /**
+      
+       Majority Element II
+        Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+
+
+    **/
+
+    [TestClass]
+    public class _229
+    {
+        public IList<int> MajorityElement(int[] nums)
+        {
+            if (nums.Length < 2) return new List<int>(nums);
+            IList<int> result = new List<int>();
+            var n1 = 0;
+            var n2 = 0;
+            var c1 = 0;
+            var c2 = 0;
+            foreach (var i in nums)
+            {
+                if (i == n1) c1++;
+                else if (i == n2) c2++;
+                else if (c1 == 0)
+                {
+                    n1 = i;
+                    c1 = 1;
+                }
+                else if (c2 == 0)
+                {
+                    n2 = i;
+                    c2 = 1;
+                }
+                else
+                {
+                    c1--;
+                    c2--;
+                }
+            }
+            c1 = 0;
+            c2 = 0;
+            foreach (var t in nums)
+            {
+                if (t == n1) c1++;
+                if (t == n2) c2++;
+            }
+            if (c1 > nums.Length / 3) result.Add(n1);
+            if (c2 > nums.Length / 3 && n2 != n1) result.Add(n2);
+            return result;
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+
+
+
+        }
+
+
+    }
+}
