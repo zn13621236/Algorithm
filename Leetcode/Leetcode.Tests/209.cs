210dfd06fb5189f1559ce8fabc39038fa8463b75
zn13621236 <nanzhao136@gmail.com>
2015-11-19 17:27:30 -0800

    update class template

---
+++ Leetcode/Leetcode.Tests/209.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leetcode.Tests
+{
+
+/**
+    Minimum Size Subarray Sum
+
+Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+
+click to show more practice.
+
+More practice:
+If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+
+**/
+
+    [TestClass]
+    public class _209
+    {
+
+
+
+
+
+        [TestMethod]
+        public void testMethod()
+        {
+
+
+
+
+        }
+
+
+    }
+}
--- Leetcode/Leetcode.Tests/NewClass1.cs
+++ Leetcode/Leetcode.Tests/NewClass1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Leetcode.Tests
 {
-    class NewClass1
+    [TestClass]
+   public class NewClass1
     {
+
+        [TestMethod]
+        public void test1()
+        {
+        }
     }
 }
